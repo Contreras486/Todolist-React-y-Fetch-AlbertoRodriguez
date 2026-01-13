d2da57ca48aefdc25e103cc928b4728147b90218
Contreras486 <contreras65.jc@gmail.com>
2026-01-13 19:05:10 +0000

    Falta por terminar

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -6,8 +6,25 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 
-	const API_URL = "https://playground.4geeks.com"
+	const API_URL = "https://playground.4geeks.com/todo"
 	const USER = "Alberto"
+
+	const [tarea, setTarea] = useState([])
+	const [newTarea, setNewTarea] = useState({
+
+		nombre: "",
+		frase: "",
+		Imagen: ""
+
+	})
+	console.log(tarea);
+
+	const handleInputsCange = (e) => {
+		setNewTarea({
+			...newTarea,
+			[e.target.name]: e.target.value
+			})
+	}
 
 	const getCharacters = async () => {
 		const response = await fetch(`${API_URL}/users/${USER}`)
@@ -19,28 +36,28 @@ const Home = () => {
 		}
 		const data = await response.json()
 		console.log(data);
-		
-		
+
+
 	}
 
 	const creaTeUser = async () => {
-		const response = await fetch(`${API_URL}/users/${USER}`,{
+		const response = await fetch(`${API_URL}/users/${USER}`, {
 			method: "POST"
 		})
 		console.log(response);
 		if (!response.ok) {
 			console.log("Debo crear un usuario");
-			
+
 			return
 		}
 		const data = await response.json()
 		console.log(data);
-		
+
 	}
 
-	useEffect(()=> {
+	useEffect(() => {
 		getCharacters()
-	},[])
+	}, [])
 
 
 	// fetch() => peticion
@@ -49,9 +66,21 @@ const Home = () => {
 	// .catch() error
 
 	return (
-		<div className="text-center">
-            <h1 className="text-center mt-5">Hello Rigo!</h1>
-			
+		<div className="container pt 4">
+			<h2 className="text-center text-light">Hello Rigo!</h2>
+			<form className="mb-5"> 
+				<input 
+				type="text"
+				className="form-control mb-2"
+				placeholder="Nueva tarea"
+				value={newTarea}
+				onChange={handleInputsCange} 
+				
+				
+				/>
+
+			</form>
+
 		</div>
 	);
 };
